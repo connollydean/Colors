082b942beac58913425038fe8576f3e9ff0dfda5
Connolly Dean <cadnrf@mail.missouri.edu>
2018-09-28 14:05:36 -0500

    first commit

---
+++ Colors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Colors
+//
+//  Created by Connolly Dean on 9/28/18.
+//  Copyright © 2018 Connolly Dean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
